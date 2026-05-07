0de07685aa1d65bbd4115b9a5a691eb421c78a4f
Abbas <kaziabbasce09@gmail.com>
2026-05-07 09:48:44 +0000

    Live demo change

--- frontend/services/api.ts
+++ frontend/services/api.ts
@@ -2,7 +2,9 @@
 import axios from 'axios';
 
 //const API_URL = 'http://127.0.0.1:32691/api';
-const API_URL = 'http://192.168.49.2:32691/api';
+//const API_URL = 'http://192.168.49.2:32691/api';
+//const API_URL = 'http://localhost:5000/api';
+const API_URL = 'http://localhost:5000/api';
 const api = axios.create({
   baseURL: API_URL,
   withCredentials: true,
